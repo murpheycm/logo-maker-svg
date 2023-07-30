b72f647fbe5a3eadbdce4b6ee744214642626c6c
murpheycm <cmmurphey@gmail.com>
2023-07-30 18:23:59 -0400

    Debugged generateSVG; moved code to index.js function and removed generateSVG.js

--- assets/shapes.js
+++ assets/shapes.js
@@ -3,31 +3,31 @@
 // Shape class with constructor to define each shape
 class Shape {
     constructor() {
-      this.color = "";
+      this.shapeColor = "";
     }
-    setColor(colorVar) {
-      this.color = colorVar;
+    setColor(color) {
+      this.shapeColor = color;
     }
   }
   
   // Triangle, Square, and Circle classes inherits Shape class
   class Triangle extends Shape {
     render() {
-      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColor}" />`;
     }
   }
   
   class Square extends Shape {
     render() {
-      return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
+      return `<rect x="73" y="40" width="160" height="160" fill="${this.shapeColor}" />`;
     }
   }
   
   class Circle extends Shape {
     render() {
-      return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
+      return `<circle cx="150" cy="115" r="80" fill="${this.shapeColor}" />`;
     }
   }
   
   // Exports Square, Triangle, and Circle classes to index.js
-  module.exports = { Triangle, Square, Circle };
+  module.exports = {Triangle, Square, Circle};
--- index.js
+++ index.js
@@ -7,25 +7,24 @@ const generateSVG = require("./assets/generateSVG.js");
 const { Triangle, Square, Circle } = require("./assets/shapes.js");
 
 //Application questions for SVG creation
-
 const questions = [
     {
         type: "list",
         name: "shape",
         message: "What shape would you like for your logo?",
-        choices: ["square", "circle", "triangle"],
+        choices: ["Square", "Circle", "Triangle"],
     },
     {
         type: "list",
         name: "shapeColor",
         message: "What color would you like the logo shape to be?",
-        choices: ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "white", "gray"],
+        choices: ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White", "Gray"],
     },
     {
         type: "list",
         name: "textColor",
         message: "What color would you like the logo text to be?",
-        choices: ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "white", "gray"],
+        choices: ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White", "Gray"],
     },
     {
         type: "input",
@@ -33,18 +32,51 @@ const questions = [
         message: "Please enter up to three letters for your logo:",
         validate: function (input) {
             if (input.length > 3) {
-                            return console.log("Please provide three or less characters:");
-                        } else {
-                            return true;
-                        }
+                return console.log("Please provide three or less characters:");
+                } else {
+                    return true;
+                }
         }
     }
 ];
 
 //Function to write the SVG file
-
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
+    const shape = data.shape;
+    const shapeColor = data.shapeColor;
+    const textColor = data.textColor;
+    const logoText = data.logoText;
+
+    //Start of SVG string to creat SVG logo
+     let svgHTML = "";
+     svgHTML = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+     svgHTML += "<g>";
+     svgHTML += `${shape}`;
+ 
+     
+     //User input --> logo shape
+     // let shapeChoice;
+     if (shape === "Triangle") {
+         // shapeChoice = new Triangle();
+         svgHTML += `<polygon points="150, 18 244, 182 56, 182" fill="${shapeColor}"/>`;
+     }
+     if (data.shape === "Square") {
+         shapeChoice = new Square();
+         svgHTML += `<rect x="73" y="40" width="160" height="160" fill="${shapeColor}"/>`;
+     };
+     if (data.shape === "Circle") {
+         shapeChoice = new Circle();
+         svgHTML += `<circle cx="150" cy="115" r="80" fill="${shapeColor}"/>`;
+     };
+ 
+     //End of SVG string; User input --> logo text
+     svgHTML += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${textColor}">${logoText}</text>`;
+     svgHTML += "</g>";
+     svgHTML += "</svg>";
+    
+    
+    
+    fs.writeFile(fileName, svgHTML, function (err) {
         if (err) {
             return console.log(err);
         } else {
@@ -57,8 +89,12 @@ function writeToFile(fileName, data) {
 
 function init() {
     inquirer.prompt(questions).then(function (data) {        
-        writeToFile("logo.svg", generateSVG(data));
+        console.log("Hit init function!");
+        writeToFile("logo.svg", data);
+
     });
 };
+
+
 
 init();
